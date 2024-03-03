29df9ee7932266537ce53090cb898c91d2840cbf
gexincoding <gexincoding@gmail.com>
2024-03-03 16:53:07 +0800

    save

--- lc/middle/0450-delete-node-in-a-bst/solution.go
+++ lc/middle/0450-delete-node-in-a-bst/solution.go
@@ -1,5 +1,43 @@
 package _450_delete_node_in_a_bst
 
+// 看作一个递归函数
+// 含义：当前来到的节点是root 需要删除key，返回删除后的BST的头节点
 func deleteNode(root *TreeNode, key int) *TreeNode {
-	return nil
+	if root == nil {
+		return nil
+	}
+	if root.Val == key {
+		if isLeaf(root) {
+			return nil
+		}
+		if leftNil(root) {
+			return root.Right
+		}
+		if rightNil(root) {
+			return root.Left
+		}
+		cur := root.Right
+		for cur.Left != nil {
+			cur = cur.Left
+		}
+		cur.Left = root.Left
+		return root.Right
+	}
+	root.Left = deleteNode(root.Left, key)
+	root.Right = deleteNode(root.Right, key)
+	return root
+}
+
+func isLeaf(node *TreeNode) bool {
+	return node != nil && node.Left == nil && node.Right == nil
+}
+
+// 左空右不空
+func leftNil(node *TreeNode) bool {
+	return node != nil && node.Left == nil && node.Right != nil
+}
+
+// 右空左不空
+func rightNil(node *TreeNode) bool {
+	return node != nil && node.Left != nil && node.Right == nil
 }
---
+++ lc/middle/0669-trim-a-binary-search-tree/solution.go
@@ -0,0 +1,5 @@
+package _669_trim_a_binary_search_tree
+
+func trimBST(root *TreeNode, low int, high int) *TreeNode {
+	return nil
+}
